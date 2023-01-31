ad0359b27e3dcfff2c59a0c69ac4794c800a326b
Tanya Engelhart <technicaltanya@gmail.com>
2023-01-31 03:08:42 -0600

    updates to package.json

---
+++ portfolio/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Footer from '../components/Footer';
+const about = () => {
+    return (
+        <div>
+           <Footer /> 
+        </div>
+    );
+};
+
+export default about;
---
+++ portfolio/pages/resume.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Footer from '../components/Footer';
+const resume = () => {
+    return (
+        <div>
+           <Footer /> 
+        </div>
+    );
+};
+
+export default resume;
