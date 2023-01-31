4b46062b810a99aaaed3b9da1eb0cabf64ef5a72
Tanya Engelhart <technicaltanya@gmail.com>
2023-01-31 15:23:20 -0600

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,7 +10,7 @@ import Project from "../components/Project";
 export default function Home() {
 	return (
     <>			<Head>
-				<title>Create Next App</title>
+				<title>Technical Tanya</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 <Header />
